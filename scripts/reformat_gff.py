ee1b4d85ad05a78084e08e47f52170cb75c18022
Varsha <vkale@ebi.ac.uk>
2024-09-11 10:50:46 +0100

    add braker support to gff reformatter

--- scripts/reformat_gff.py
+++ scripts/reformat_gff.py
@@ -2,13 +2,13 @@ import sys
 import argparse
 
 def reformat_metaeuk(gff, sample_id):
-    with open(gff, 'r') as metaeuk_gff_in, open(f'{sample_id}.reformatted.gff', 'w') as metaeuk_gff_out:
+    with open(gff, 'r') as metaeuk_gff_in, open(f'{sample_id}.metaeuk.reformatted.gff', 'w') as metaeuk_gff_out:
         gff_contents = metaeuk_gff_in.read()
         gff_contents = gff_contents.replace("Target_ID=", "ID=")
         metaeuk_gff_out.write(gff_contents)
 
-def reformat_galba(gtf, underscore_count, sample_id):
-    with open(gtf, 'r') as galba_gtf_in, open(f'{sample_id}.reformatted.gtf', 'w') as galba_gtf_out:
+def reformat_galba_braker(gtf, underscore_count, sample_id, tool_id):
+    with open(gtf, 'r') as galba_gtf_in, open(f'{sample_id}.{tool_id}.reformatted.gtf', 'w') as galba_gtf_out:
         for line in galba_gtf_in:
             scaffold = line.split('\t')[0]
             line_data = '\t'.join(line.split('\t')[1:])
@@ -20,11 +20,17 @@ if __name__ == "__main__":
         description="script to rename gff and gtf inputs from eukaryotic gene annotators")
     parser.add_argument("-s", "--sample")
     parser.add_argument("-g", "--galba_gtf")
-    parser.add_argument("-n", "--sep_num", description="number of underscores which contain the scaffold ID. usually 1 or 2")
+    parser.add_argument("-b", "--braker_gtf")
+    parser.add_argument("-n", "--sep_num")
     parser.add_argument("-m", "--metaeuk_gff")
     args = parser.parse_args()
+    if args.galba_gtf or args.braker_gtf and not args.sep_num:
+        print('argument -n/--sep_num is needed to shorrten the scaffold name')
+        exit
     if args.metaeuk_gff:
         reformat_metaeuk(args.gff, args.sample)
     if args.galba_gtf:
-        reformat_galba(args.galba_gtf, int(args.sep_num), args.sample)
+        reformat_galba_braker(args.galba_gtf, int(args.sep_num), args.sample, 'galba')
+    if args.braker_gtf:
+        reformat_galba_braker(args.braker_gtf, int(args.sep_num), args.sample, 'braker')
     
