1d2248f85627e224ef0110c5a869b6acfbd3b7f5
Varsha <vkale@ebi.ac.uk>
2024-09-13 14:46:50 +0100

    remove underscore number as user input and infer from reference gff

--- bin/reformat_gff.py
+++ bin/reformat_gff.py
@@ -1,5 +1,13 @@
-#!/usr/bin/env python3
+import sys
 import argparse
+
+def num_underscores(ref_gff):
+    with open(ref_gff, 'r') as reference:
+        for line in reference:
+            if not line.startswith('#'):
+                scaffold = line.split('\t')[0]
+                underscore_num = scaffold.count('_')
+                return underscore_num + 1
 
 def reformat_metaeuk(gff, sample_id):
     with open(gff, 'r') as metaeuk_gff_in, open(f'{sample_id}.metaeuk.reformatted.gff', 'w') as metaeuk_gff_out:
@@ -10,7 +18,6 @@ def reformat_metaeuk(gff, sample_id):
 def reformat_galba_braker(gtf, underscore_count, sample_id, tool_id):
     with open(gtf, 'r') as galba_gtf_in, open(f'{sample_id}.{tool_id}.reformatted.gtf', 'w') as galba_gtf_out:
         for line in galba_gtf_in:
-            # TODO: resolve names and underscores
             scaffold = line.split('\t')[0]
             line_data = '\t'.join(line.split('\t')[1:])
             simplified = '_'.join(scaffold.split('_')[0:underscore_count])
@@ -18,19 +25,19 @@ def reformat_galba_braker(gtf, underscore_count, sample_id, tool_id):
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(
-        description="script to rename gff and gtf inputs from eukaryotic gene annotators")
+        description="script to rename gff and gtf inputs from eukaryotic gene annotators to match the reference gff")
     parser.add_argument("-s", "--sample")
+    parser.add_argument("-r", "--ref_gff")
     parser.add_argument("-g", "--galba_gtf")
     parser.add_argument("-b", "--braker_gtf")
-    parser.add_argument("-n", "--sep_num", default=2, type=int)
     parser.add_argument("-m", "--metaeuk_gff")
     args = parser.parse_args()
-    if args.galba_gtf or args.braker_gtf and not args.sep_num:
-        print('argument -n/--sep_num is needed to shorrten the scaffold name. Using default')
+    sep_num = num_underscores(args.ref_gff)
     if args.metaeuk_gff:
         reformat_metaeuk(args.metaeuk_gff, args.sample)
     if args.galba_gtf:
-        reformat_galba_braker(args.galba_gtf, int(args.sep_num), args.sample, 'galba')
+        reformat_galba_braker(args.galba_gtf, sep_num, args.sample, 'galba')
     if args.braker_gtf:
-        reformat_galba_braker(args.braker_gtf, int(args.sep_num), args.sample, 'braker')
+        reformat_galba_braker(args.braker_gtf, sep_num, args.sample, 'braker')
     
+
